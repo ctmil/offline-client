6bd2471de676db63b75cc787686680d71e146a3d
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-11 17:05:22 -0300

    Added components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: '<h1>{{title}}</h1><h2>{{ticket.name}} details!</h2>'
 })
+
 export class AppComponent {
 	title = 'Cliente Off-line para Odoo';
-	hero = 'Windstorm';
+	ticket = '0001-12345678';
  }
+export class Ticket {
+	id: number;
+	name: string;
+}
 
+ticket: Ticket = {
+	id: 1,
+	name: '0001-12345678'
+	}
