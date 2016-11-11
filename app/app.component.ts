caa7d5752d66f4147261d3904b069eec0b75e711
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-11 16:56:08 -0300

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+})
+export class AppComponent {
+	title = 'Cliente Off-line para Odoo';
+	hero = 'Windstorm';
+ }
+
