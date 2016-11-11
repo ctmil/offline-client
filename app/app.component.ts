f2c151b7ba9acb5f4ce013cf42c9c928d63c4ea4
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-11 18:39:28 -0300

    Added detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,23 +5,100 @@ export class Ticket {
 	name: string;
 }
 
+const TICKETS: Ticket[] = [
+  { id: 11, name: '0001-00000001' },
+  { id: 12, name: '0001-00000002' },
+  { id: 13, name: '0001-00000003' },
+  { id: 14, name: '0001-00000004' },
+  { id: 15, name: '0001-00000005' },
+  { id: 16, name: '0001-00000006' },
+  { id: 17, name: '0001-00000007' },
+  { id: 18, name: '0001-00000008' },
+  { id: 19, name: '0001-00000009' },
+  { id: 20, name: '0001-00000010' }
+];
+
+
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-		<h2>{{ticket.name}} details!</h2>
-		<div><label>id: </label>{{ticket.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="ticket.name" placeholder="name">
-		</div>`
+		<h2>My Tickets</h2>
+		<ul class="heroes">
+			<li *ngFor="let ticket of tickets" 
+				[class.selected]="ticket === selectedTicket"
+				(click)="onSelect(ticket)">
+				<!-- each ticket goes here -->
+				<span class="badge">{{ticket.id}}</span> {{ticket.name}}
+			</li>
+		</ul>
+		<div *ngIf="selectedTicket">
+		      <h2>{{selectedTicket.name}} details!</h2>
+		      <div><label>id: </label>{{selectedTicket.id}}</div>
+		      <div>
+		        <label>name: </label>
+		        <input [(ngModel)]="selectedTicket.name" placeholder="name"/>
+		      </div>
+		</div>
+		`
+styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 
 export class AppComponent {
 	title = 'Cliente Off-line para Odoo';
-	/* ticket = '0001-12345678'; */
-	ticket: Ticket = {
-		id: 1,
-		name: '0001-87654321'
-		};
+	tickets = TICKETS;
+	selectedTicket: Ticket;
+
+	onSelect(ticket: Ticket): void {
+	  this.selectedTicket = ticket;
+	}
+
 	}
 
