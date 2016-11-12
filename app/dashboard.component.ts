7cd58cd12c57556b9fee10af1e70158f1f4544cf
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-12 16:28:33 -0300

    Mega commit

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -5,6 +5,7 @@ import { TicketService } from './ticket.service';
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'dashboard.component.html',
+  styleUrls: [ 'dashboard.component.css' ]
 })
 
 export class DashboardComponent implements OnInit {
