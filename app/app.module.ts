0061a1745b7a40f520acdf865d64342a06e643f9
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-12 12:45:59 -0300

    Added dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,20 +8,31 @@ import { AppComponent }   from './app.component';
 import { TicketDetailComponent } from './ticket-detail.component';
 import { TicketsComponent } from './ticket.component';
 import { TicketService } from './ticket.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports:      [ 
 	BrowserModule,
 	FormsModule,
 	RouterModule.forRoot([
+		{
+			path: '',
+			redirectTo: '/dashboard',
+			pathMatch: 'full'
+		},
 		  {
 		    path: 'tickets',
 		    component: TicketsComponent
-		  }
+		  },
+		  {
+		    path: 'dashboard',
+		    component: DashboardComponent
+		  },
 		])
 	],
   declarations: [ 
 		AppComponent,
+		DashboardComponent,
 		TicketDetailComponent,
 		TicketsComponent
 		],
