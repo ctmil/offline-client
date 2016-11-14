310d3c4d0a6b83112fd5a981bd4d2d9cf6ac1a5e
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-14 19:43:09 -0300

    Added bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard">Dashboard</a>
-      <a routerLink="/tickets">Tickets</a>
-    </nav>
-    <router-outlet></router-outlet>
+	<div class="container">
+            <div id="navbar" class="collapse navbar-collapse">
+	          <ul class="nav navbar-nav">
+		      <li class="active"><a routerLink="/dashboard">Dashboard</a></li>
+		      <li><a routerLink="/tickets">Tickets</a></li>
+	          </ul>
+            </div>
+	    <h1>{{title}}</h1>
+	    <router-outlet></router-outlet>
+	</div>
   `
 })
 export class AppComponent {
