ecb83224552e25e56a958905fcb8b6556400c75a
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-14 19:45:32 -0300

    Changed nav structure

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="container">
+	            <div id="navbar" class="collapse navbar-collapse">
+		          <ul class="nav navbar-nav">
+			      <li class="active"><a routerLink="/dashboard">Dashboard</a></li>
+			      <li><a routerLink="/tickets">Tickets</a></li>
+		          </ul>
+	            </div>
+	      </div>
+	</nav>
 	<div class="container">
-            <div id="navbar" class="collapse navbar-collapse">
-	          <ul class="nav navbar-nav">
-		      <li class="active"><a routerLink="/dashboard">Dashboard</a></li>
-		      <li><a routerLink="/tickets">Tickets</a></li>
-	          </ul>
-            </div>
 	    <h1>{{title}}</h1>
 	    <router-outlet></router-outlet>
 	</div>
